b10042fe3afdbf72141207ab0cb067553a70d356
matiz22 <mateuszmalich@gmail.com>
2023-03-12 23:55:09 +0100

    Logowanie

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'dart:math';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:projekt_zaliczeniowy/screens/LoginPage.dart';
 import 'package:projekt_zaliczeniowy/screens/PhotosScreen.dart';
 import 'package:projekt_zaliczeniowy/screens/PostsScreen.dart';
 import '../model/Post.dart';
+import 'firebase_options.dart';
 
 Color randomColor() {
   var generatedColor = Random().nextInt(Colors.primaries.length);
   return Colors.primaries[generatedColor];
 }
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     routes: {
-      "/": (context) => PostsScreen(),
+      "/": (context) => const LoginPage(),
+      "/posts": (context) => PostsScreen(),
       "/photos" : (context) => const PhotoScreen(),
       "/album" : (context) => const PhotoScreen()
     },
--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'PostsScreen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,31 +12,126 @@ class LoginPage extends StatefulWidget {
 }
 
 class _MyLoginPage extends State<LoginPage> {
-  bool login = true;
+  bool isMember = true;
+  final email = TextEditingController();
+  final password = TextEditingController();
 
-  void _incrementCounter() {
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+  void _changeType() {
     setState(() {
-      login != login;
+      isMember = !isMember;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
+    return Scaffold(
+      body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Card(
                 child: Column(
               children: [
-                if (login) const Text('Zaloguj sie'),
-                if (!login) const Text('Zarejstruj sie'),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: email,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter valid mail id as abc@gmail.com'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: password,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter your secure password'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: FilledButton(
+                    child: isMember
+                        ? const Text("Zaloguj sie")
+                        : const Text("Zarejestruj sie"),
+                    onPressed: () async {
+                      if (isMember) {
+                        try {
+                          final credential = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email.text, password: password.text);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PostsScreen()));
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          }
+                        }
+                      } else {
+                        try {
+                          final credential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                            email: email.text,
+                            password: password.text,
+                          );
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PostsScreen()));
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('The password provided is too weak.');
+                          } else if (e.code == 'email-already-in-use') {
+                            print('The account already exists for that email.');
+                          }
+                        } catch (e) {
+                          print(e);
+                        }
+                      }
+                    },
+                  ),
+                ),
+                if (isMember)
+                  (Padding(
+                    padding: EdgeInsets.all(30),
+                    child: OutlinedButton(
+                      child: Text("Nie masz konta Zarejstruj sie"),
+                      onPressed: () {
+                        _changeType();
+                      },
+                    ),
+                  ))
+                else
+                  (Padding(
+                    padding: EdgeInsets.all(30),
+                    child: OutlinedButton(
+                      child: Text("Masz konto zaloguj sie"),
+                      onPressed: () {
+                        _changeType();
+                      },
+                    ),
+                  )),
               ],
             ))
           ]),
-    ));
+    );
   }
 }
